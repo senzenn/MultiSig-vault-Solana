f9e4f79c6288c519f621b2c927eaaaad26d82195
senzenn <ascenzen11@gmail.com>
2025-09-06 07:51:36 +0530

    Clean up unused files and add .gitignore

--- target/debug/deps/vault_program-0b0a8a5bf139e54a.d
+++
@@ -1,5 +0,0 @@
-/Users/proxymita/Projects/Rust/webdev-rust/vault-solana/vault_program/target/debug/deps/vault_program-0b0a8a5bf139e54a.d: src/lib.rs
-
-/Users/proxymita/Projects/Rust/webdev-rust/vault-solana/vault_program/target/debug/deps/libvault_program-0b0a8a5bf139e54a.rmeta: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/vault_program-94d294a9f08b04ab.d
+++
@@ -1,9 +0,0 @@
-/Users/proxymita/Projects/Rust/webdev-rust/vault-solana/vault_program/target/debug/deps/vault_program-94d294a9f08b04ab.d: src/lib.rs Cargo.toml
-
-/Users/proxymita/Projects/Rust/webdev-rust/vault-solana/vault_program/target/debug/deps/libvault_program-94d294a9f08b04ab.rmeta: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__-W__CLIPPY_HACKERY__clippy::all__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
